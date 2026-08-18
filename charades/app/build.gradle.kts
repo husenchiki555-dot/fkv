e942623a8d59699f46a05e622ea2d0f8ce8b508b
husenchiki555-dot <husenchiki555@gmail.com>
2026-08-18 12:53:42 +0200

    Add Party Charades app build config

---
+++ charades/app/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.partycharades.offline"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.partycharades.offline"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
